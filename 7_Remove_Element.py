04ce7e5bbb0e030a2f95c0c17a82f5115d61dce4
Jessie B <79630573+A109010048@users.noreply.github.com>
2023-03-07 23:08:32 +0800

    Add files via upload

---
+++ 7_Remove_Element.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+
+        k = 0
+        for i in range(len(nums)) :
+            if nums[i] == val:
+                continue
+            nums[k] = nums[i]
+            k += 1
+        return k
+    
