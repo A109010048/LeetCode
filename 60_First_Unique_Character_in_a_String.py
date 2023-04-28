aa326ae75256454e096597f75152dbbe48b07fc2
Jessie B <79630573+chieh28@users.noreply.github.com>
2023-04-28 21:51:10 +0800

    Add files via upload

---
+++ 60_First_Unique_Character_in_a_String.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def firstUniqChar(self, s):
+        dic = {}
+        for c in s:
+            if c not in dic:
+                dic[c] = 1
+            else:
+                dic[c] += 1
+
+        for i in range(len(s)):
+            if dic[s[i]] == 1:
+                return i 
+
+        return -1
