62ef29fb7ad53c909bad7aeb6432eacbd0b504bf
Jessie B <79630573+chieh28@users.noreply.github.com>
2023-08-12 23:20:32 +0800

    Add files via upload

---
+++ 105_Array_Partition.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def arrayPairSum(self, nums):
+        nums.sort()
+        sum = 0
+        for i in range(0, len(nums), 2):
+            sum += nums[i]
+
+        return sum
